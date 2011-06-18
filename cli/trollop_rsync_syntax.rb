35aa0eef2b381ed1d3011a322028e6536580a366
molecule <root@erikj.info>
2011-06-17 23:33:55 -0600

    adding handling of second filename input; improving split() value tests; updating banner

--- cli/trollop_rsync_syntax.rb
+++ cli/trollop_rsync_syntax.rb
@@ -4,10 +4,10 @@ require 'trollop'
 def parse_filename(filename)
   parsed = { :path=>filename }
 
-  if filename.split(':').length > 1
-    parsed[:server], parsed[:path] = filename.split(':')
-    if parsed[:server].split('@').length > 1
-      parsed[:user], parsed[:server] = filename.split('@')
+  if filename.split(':').length == 2
+    parsed[:host], parsed[:path] = filename.split(':')
+    if parsed[:host].split('@').length == 2
+      parsed[:user], parsed[:host] = parsed[:host].split('@')
     end
   end
 
@@ -17,7 +17,7 @@ end
 def trollop_example argv
 
   global_opts = Trollop::options do
-    banner "magic file deleting and copying utility"
+    banner "demo to emulate rsync / scp syntax"
     opt :dry_run, "Don't actually do anything", :short => "-n"
     # stop_on SUB_COMMANDS
   end
@@ -30,6 +30,9 @@ def trollop_example argv
 
   if argv.length >= 1
     puts "TODO: parse argv0: %s" % parse_filename(argv[0]).inspect
+    if argv.length >= 2
+      puts "TODO: parse argv1: %s" % parse_filename(argv[1]).inspect
+    end
 
     # puts "TODO: parse argv1: argv1 %s" % argv[1]
   end
