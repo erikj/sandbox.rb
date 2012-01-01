526ddf05df2aeca11c83fd3d7425587c602e7629
erikj <root@erikj.info>
2012-01-01 16:08:39 -0700

    adding demo code to read / write from stations collection;

--- mongodb/mongoid/finder.rb
+++ mongodb/mongoid/finder.rb
@@ -1,4 +1,22 @@
-$:.unshift '.'
+$:.unshift File.dirname(__FILE__)
 
 require 'models'
 
+# Mongoid.load!("mongoid.yml")
+
+Mongoid.configure do |config|
+  config.master = Mongo::Connection.new.db("godfather")
+end
+
+station_fields = {
+  latitude: 40.0,
+  longitude: -120,
+  zip: 80301,
+  location_code: :GUNB
+}
+
+station = Station.new station_fields
+
+station.save
+require 'pp'
+Station.all().each{ |s| pp s }
--- mongodb/mongoid/models.rb
+++ mongodb/mongoid/models.rb
@@ -1,2 +1,41 @@
 require 'rubygems'
 require 'mongoid'
+
+class Station
+  include Mongoid::Document
+  field :latitude,      :type=> Float
+  field :longitude,     :type=> Float
+  field :zip,           :type => Integer
+  field :location_code, :type => String
+  # has_many :forecasts
+  # has_many :observations
+  # has_many :alerts
+end
+
+# class Forecast
+#   include Mongoid::Document
+#   include Mongoid::Timestamps
+#   field :forecast_start, :type=>DateTime
+#   field :forecast_end,   :type=>DateTime
+#   belongs_to :station
+#   has_many :observations
+#   validates_presence_of :station
+# end
+
+# class Observation
+#   include Mongoid::Document
+#   include Mongoid::Timestamps
+#   field :observation_date, :type=>DateTime
+#   belongs_to :station
+#   belongs_to :forecast
+#   validates_presence_of :station
+# end
+
+# class Alert
+#   include Mongoid::Document
+#   include Mongoid::Timestamps
+#   field :alert_start, :type=>DateTime
+#   field :alert_end,   :type=>DateTime
+#   belongs_to :station
+#   validates_presence_of :station
+# end
